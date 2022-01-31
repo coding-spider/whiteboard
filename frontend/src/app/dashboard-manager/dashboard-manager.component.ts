e7b475322c42372e07fca827c6d6772dfbf7ea03
rohan <rkoli@zscaler.com>
2022-01-31 21:58:20 +0530

    feat: grid is working now

--- frontend/src/app/dashboard-manager/dashboard-manager.component.ts
+++ frontend/src/app/dashboard-manager/dashboard-manager.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GridStack, GridStackWidget } from 'gridstack';
+import 'gridstack/dist/h5/gridstack-dd-native';
+
 @Component({
   selector: 'app-dashboard-manager',
   templateUrl: './dashboard-manager.component.html',
-  styleUrls: ['./dashboard-manager.component.css']
+  styleUrls: [
+    './dashboard-manager.component.css'
+  ]
 })
 export class DashboardManagerComponent implements OnInit {
 
-  constructor() { }
+  private items: GridStackWidget[] = [
+    { x: 0, y: 0, w: 9, h: 6, content: '0' },
+    { x: 9, y: 0, w: 3, h: 3, content: '1' },
+    { x: 9, y: 3, w: 3, h: 3, content: '2' },
+  ];
+  private grid: GridStack | undefined;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  // simple div above doesn't require Angular to run, so init gridstack here
+  public ngOnInit() {
+    this.grid = GridStack.init({
+      cellHeight: 70,
+    })
+      .load(this.items); // and load our content directly (will create DOM)
+  }
+
+  public add() {
+    this.grid?.addWidget({w: 3, content: 'new content'});
+  }
+  public delete() {
+    // this.grid.removeWidget(this.grid.engine.nodes[0].el);
+  }
+  public change() {
+    // this.grid.update(this.grid.engine.nodes[0].el, {w: 1});
   }
 
 }
